29de5cfca87f7c1fa6d92d5584b0ded4c40e6560
anshKjha10 <anshKjha10@users.noreply.github.com>
2026-05-10 11:20:30 +0530

    backend ready

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,26 @@
-const express = require('express');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const authRoutes = require("./routes/auth.routes");
+const eventRoutes = require("./routes/event.routes");
+const regisRoutes = require("./routes/registration.routes");
 
+const cors = require("cors");
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.use(cors({
+    origin: 'http://localhost:5173', // frontend URL
+    credentials: true    // allows cookies to be sent
+}));
+
+app.use(express.json());  // body parser middleware
+app.use(cookieParser());  // cookie parser middleware
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Eventify API.");
 })
+
+app.use('/api/auth', authRoutes)
+app.use('/api/event', eventRoutes)
+app.use('/api/registration', regisRoutes)
 
 module.exports = app;
